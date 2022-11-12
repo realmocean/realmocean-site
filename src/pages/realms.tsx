c1143eeb6ebdec3af36de741c4f2f687b76c1414
selimtan <stanoncloud@gmail.com>
2022-11-12 16:02:46 +0300

    .

--- src/pages/realms.tsx
+++ src/pages/realms.tsx
@@ -1,26 +1,40 @@
 import React, { useState } from 'react';
 import Layout from '@theme/Layout';
 
+const realms = [
+  {
+    name: 'peda360',
+    image_url: 'https://peda360.com/img/peda360-logo.png',
+    realm_link: 'http://apps.peda360.com'
+  },
+  {
+    name: 'Next Normal'
+  },
+  {
+    name: 'BPM Genesis'
+  }
+]
 export default function AppStore() {
   const [test, setTest] = useState('AAA');
   return (
     <Layout title="Hello" description="Hello React Page">
-      {
-       Array.from({length: 10}, () => Math.floor(Math.random() * 100)).map(item => (
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '50vh',
-              fontSize: '20px',
-            }}>
-            <p>
-              Realm {item} {test}
-            </p>
-          </div>
-        ))
-      }
+      <div className="grid-container">
+        {
+          realms.map(item =>
+          (
+            <div className="grid-item">
+              <span>{item.name}</span>
+              <img src={item.image_url}></img>
+              <div className="spacer"></div>
+              <a target={'_blank'} href={item.realm_link}>Go to realm</a>
+
+            </div>
+          )
+
+          )
+        }
+      </div>
+
 
     </Layout>
   );
